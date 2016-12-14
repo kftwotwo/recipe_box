9f1ca3502beb68b16bc9199203a6994b609d0ca7
Josh Rinard and Kevin Finley <josh@>
2016-12-14 08:33:53 -0800

    initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+require('rspec')
+require('pg')
+require('sinatra/activerecord')
+require('./lib/recipe')
+require('./lib/tag')
+
+RSpec.configure do |config|
+  config.after(:each) do
+
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+
+    Tag.all().each() do |tag|
+      tag.destroy()
+    end
+  end
+end
